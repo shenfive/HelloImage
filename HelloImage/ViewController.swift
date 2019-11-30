4274127bb42b390e8b859c3f95692e7cf5ec3cd9
Danny Shen <shenfive@gmail.com>
2019-11-30 11:22:53 +0800

    image init

--- HelloImage/ViewController.swift
+++ HelloImage/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let image = Bundle.main.path(forResource: "test", ofType: "jpg"){
+            myImageView.image = UIImage.init(contentsOfFile: image)
+        }
+        
+        
     }
 
 
